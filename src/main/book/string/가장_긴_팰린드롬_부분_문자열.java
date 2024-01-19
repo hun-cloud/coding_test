580a0fc45c977f4d17054a35df6b5b69d69271c4
leejaehun <ljh0901@mlsoft.com>
2024-01-19 16:07:03 +0900

    fix : 문제 다시 스스로 풀어보기

--- src/main/book/string/가장_긴_팰린드롬_부분_문자열.java
+++ src/main/book/string/가장_긴_팰린드롬_부분_문자열.java
@@ -4,40 +4,40 @@ public class 가장_긴_팰린드롬_부분_문자열 {
     public static int left = 0, maxLen = 0;
 
     public static void main(String[] args) {
+        String str = "dcbabcdd";
 
+        String result = longestPalindrome(str);
+        System.out.println("result = " + result);
     }
     /*
-    * 가장 긴 패린드롬 부분 문자열을 출력하라
-    * in : dcbabcdd
-    * out : dcbabcd
-    * */
+     * 가장 긴 패린드롬 부분 문자열을 출력하라
+     * in : dcbabcdd
+     * out : dcbabcd
+     * */
+    private static String longestPalindrome(String str) {
 
-    public static String 부분_문자열_구하기() {
-
-        String s = "dcbabcdd";
-
-        int len = s.length();
-        if (len < 2) {
-            return s;
+        if (str.length() < 2) {
+            return str;
         }
-        for (int i = 0; i < len -1; i++) {
-            extendPalindrome(s, i, i + 1);
-            extendPalindrome(s, i, i + 2);
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            extendPalindrome(str, i, i + 1);
+            extendPalindrome(str, i, i + 2);
         }
-        return s.substring(left, left + maxLen);
+
+        return str.substring(left, left + maxLen);
     }
 
-    public static void extendPalindrome(String s, int j, int k) {
-        // 투 포인터가 유효한 범위 내에 있고 양쪽 끝 문자가 일치하는 팰린드롬인 경우 범위 확장
-        while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
-            j--;
-            k++;
+    private static void extendPalindrome(String str, int x, int y) {
+        
+        while (x >= 0 && y < str.length() && str.charAt(x) == str.charAt(y)) {
+            x--;
+            y++;
         }
-
-        // 기존 최대 길이보다 큰 경우 값 교체
-        if (maxLen < k - j - 1) {
-            left = j + 1;
-            maxLen = k - j - 1;
+        
+        if (maxLen < y - x - 1) {
+            left = x + 1;
+            maxLen = y - x - 1;
         }
     }
 }
