96ca196a8346afdad732b72503f12b93cdbdf754
jayjaehun <jhl9838@naver.com>
2024-12-05 23:15:47 +0900

    codingtes : 과일가져가기

---
+++ src/main/inflearn/시뮬레이션과구현/과일가져가기.java
@@ -0,0 +1,74 @@
+package main.inflearn.시뮬레이션과구현;
+
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class 과일가져가기 {
+    public static void main(String[] args) {
+        int[][] fruit = {{10, 20, 30}, {12, 15, 20}, {20, 12, 15}, {15, 20, 10},
+                {10, 15, 10}};
+        int solution = solution(fruit);
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int[][] fruit) {
+
+        int[] ch = new int[fruit.length];
+        for (int i = 0; i < fruit.length; i++) {
+            if (ch[i] == 1) continue;
+            int[] fruitArr = fruit[i];
+            int min = getMin(fruitArr);
+            if (isNotUniqueMin(fruitArr, min)) continue;
+
+            for (int j = i + 1; j < fruit.length; j++) {
+                if (ch[i] == 1) continue;
+                int[] comparativeFruit = fruit[j];
+                int comparativeFruitMin = getMin(comparativeFruit);
+                if (isNotUniqueMin(comparativeFruit, comparativeFruitMin)) continue;
+                int a = getMinIndex(fruitArr, min);
+                int b = getMinIndex(comparativeFruit, comparativeFruitMin);
+
+                if (a != b && fruit[i][b] > 0 && fruit[j][a] > 0) {
+                    if (fruit[i][a] + 1 <= fruit[i][b] && fruit[j][b] + 1 <= fruit[j][a] - 1) {
+                        fruit[i][a]++;
+                        fruit[i][b]--;
+                        fruit[j][b]++;
+                        fruit[j][a]--;
+                        ch[i] = 1;
+                        ch[j] = 1;
+                        break;
+                    }
+                }
+
+            }
+
+        }
+
+        return Arrays.stream(fruit)
+                .mapToInt(i -> getMin(i))
+                .sum();
+    }
+
+    private static int getMinIndex(int[] fruitArr, int min) {
+
+        return IntStream.range(0, fruitArr.length)
+                .filter(i -> fruitArr[i] == min)
+                .findFirst()
+                .orElse(-1);
+    }
+
+    private static boolean isNotUniqueMin(int[] fruitArr, int min) {
+        return Arrays.stream(fruitArr)
+                .filter(v -> v == min)
+                .count() != 1;
+    }
+
+    private static int getMin(int[] fruitArr) {
+        return Arrays.stream(fruitArr)
+                .min()
+                .getAsInt();
+    }
+
+
+}
