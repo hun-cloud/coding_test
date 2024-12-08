a899c3b321055951e63cc045def67be4e57476fd
jayjaehun <jhl9838@naver.com>
2024-12-08 14:53:27 +0900

    codingtest : 음수가있 있는 부분수열

---
+++ src/main/inflearn/해싱시간파싱/C_음수가있는부분수열.java
@@ -0,0 +1,27 @@
+package main.inflearn.해싱시간파싱;
+
+public class C_음수가있는부분수열 {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3,-3, 1, 2};
+        int m = 3;
+        int solution = solution(nums, m);
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(int[] nums, int m) {
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            if (sum == m) result++;
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == m) {
+                    result++;
+                }
+            }
+        }
+
+        return result;
+    }
+}
