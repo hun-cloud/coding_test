cea59383bf0519e60d1f2b9a312a90665e6b678e
leejaehun <ljh0901@mlsoft.com>
2024-12-12 13:50:55 +0900

    codingtest : 서로 다른 빈도수 만들기

---
+++ src/main/inflearn/해싱시간파싱/C_서로다른빈도수만들기.java
@@ -0,0 +1,41 @@
+package main.inflearn.해싱시간파싱;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class C_서로다른빈도수만들기 {
+    public static void main(String[] args) {
+        String s = "aaabbbcccdddeeeeeff";
+        int solution = solution(s);
+        System.out.println("solution = " + solution);
+    }
+
+    public static int solution(String s) {
+        int result = 0;
+        char[] charArray = s.toCharArray();
+        Map<Character, Integer> cMap = new HashMap<>();
+        int max = 0;
+
+        for (char c : charArray) {
+            cMap.put(c, cMap.getOrDefault(c, 0) + 1);
+            max = Math.max(max, cMap.get(c));
+        }
+
+        int[] countArray = cMap.values().stream().mapToInt(i -> i)
+                .toArray();
+
+        Arrays.sort(countArray);
+
+        for (int i = countArray.length - 1; i > 0; i--) {
+            if (countArray[i] <= countArray[i - 1]) {
+                while (countArray[i] <= countArray[i - 1] && countArray[i - 1] != 0) {
+                    countArray[i - 1]--;
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(countArray));
+        return result;
+    }
+}
